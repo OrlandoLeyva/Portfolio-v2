5f8f66a018b308bdd8d1102e8a0737fab44b2a46
orlando leyva <leyvaorlandomx08@gmail.com>
2023-06-06 14:37:27 -0700

    improving nav bar experience

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const nav = document.getElementById('nav')
 const menuIcon = document.getElementById('menu-icon')
+const navbarItems = document.querySelectorAll('#nav-items li a')
+
+navbarItems.forEach(item => {
+    item.addEventListener('click', ()=>{
+        nav.classList.remove('active')
+    })
+})
+
+console.log(navbarItems);
 
 // TOGGLE NAV
 menuIcon.addEventListener('click', ()=>{
